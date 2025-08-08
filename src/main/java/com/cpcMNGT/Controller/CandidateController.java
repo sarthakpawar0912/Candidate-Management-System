b570b93f10acfeb5c67f5e7a04045430fba37287
sarthakpawar0912 <pawarsr06@gmail.com>
2025-08-08 10:24:11 +0530

    Fixed bugs and updated code

--- src/main/java/com/cpcMNGT/Controller/CandidateController.java
+++ src/main/java/com/cpcMNGT/Controller/CandidateController.java
@@ -4,7 +4,6 @@ import com.cpcMNGT.Service.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
